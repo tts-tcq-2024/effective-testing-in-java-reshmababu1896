6d573d2855a58dc71eff16fd049f81e89e7abdac
reshmababu1896 <reshmababu1896@gmail.com>
2024-08-26 11:08:55 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -15,13 +15,13 @@ public class Alerter {
             // let us keep a count of failures to report
             // However, this code doesn't count failures!
             // Add a test below to catch this bug. Alter the stub above, if needed.
-            alertFailureCount += 1;
+            alertFailureCount += 0;
         }
     }
     public static void main(String[] args) {
         alertInCelcius(400.5f);
         alertInCelcius(303.6f);
-        assert( alertFailureCount == 0);
+        assert( alertFailureCount == 1);
         System.out.printf("%d alerts failed.\n", alertFailureCount);
         System.out.println("All is well (maybe!)\n");
     }
